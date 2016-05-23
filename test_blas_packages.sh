5be0faca1f680f947cab49c5f55ead6f5e84fa27
Matthew Brett <matthew.brett@gmail.com>
2016-05-23 15:23:31 -0400

    Fix install error for statsmodels

--- test_blas_packages.sh
+++ test_blas_packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Test BLAS-related stack
+set -e
 export OPENBLAS_VERBOSE=2
 if [ -n "$CORE" ]; then export OPENBLAS_CORETYPE=$CORE; fi
 python -c 'import numpy; numpy.test("full")'
@@ -12,5 +13,9 @@ nosetests --exe -A "not slow and not network and not disabled"
   -e test_parsers -e TestDatetimeParsingWrappers
   -e test_range_slice_seconds -e test_range_slice_day
   pandas
+# Statsmodels latest release breaks with latest pandas; use recent trunk
+# commit
+pip install cython
+pip install https://github.com/statsmodels/statsmodels/archive/c89da5cb7c49c7bdd8699df6e7073ace43bd5250.zip
 # Exclude extremely slow statsmodels tests
 nosetests -e test_vcomp_2 -e test_sarimax --verbosity=3 statsmodels
